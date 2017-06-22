39a6763af36307e3f54b25d60d667ade3fceb73c
Tarcisio <tarcisio_df@hotmail.com>
2017-06-22 18:23:55 -0300

    last commit before macOs

--- Component/row.js
+++ Component/row.js
@@ -1,71 +1,99 @@
 import React, { Component } from 'react';
-import { AppRegistry, View ,StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
-import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
-import Pagina from '../Pages/paginanorma';  
-
+import {
+  AppRegistry,
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import {
+  Container,
+  Header,
+  Title,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon
+} from 'native-base';
+import Pagina from '../Pages/paginanorma';
+import Sound from 'react-native-sound';
+var x = require('./audio.mp3');
 
 export default class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props){
-        super(props);
-        
-    }
+  render() {
+    console.log('Entrei no render');
+    var whoosh = new Sound(x, error => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
 
-    render(){
-        
-        const { navigate }  = this.props.data.x.navigation; 
+      whoosh.play();
+      whoosh.setVolume(1);
 
-        return( 
-            
-            <TouchableOpacity  style={styles.row} onPress={() => navigate('Pagina' , {name : this.props.data.data.name })}
-                  title="Chat with Lucy">
+      // loaded successfully
+      console.log(
+        'duration in seconds: ' +
+          whoosh.getDuration() +
+          'number of channels: ' +
+          whoosh.getNumberOfChannels()
+      );
+    });
 
-                <View style={styles.image}>
-                      <Image   source={{uri : this.props.data.data.image }} style={{width: 64, height: 64}}/>
-                </View>
-              
-                <View style={styles.carac}>
-                    <Text style={styles.name}>{this.props.data.data.name}</Text>
-                    <Text style={styles.descr}>{this.props.data.data.descr} </Text>                      
-                </View>       
-            </TouchableOpacity>
+    const { navigate } = this.props.data.x.navigation;
 
-        );
-    }
-    
+    return (
+      <TouchableOpacity
+        style={styles.row}
+        onPress={() => navigate('Pagina', { name: this.props.data.data.name })}
+        title="Chat with Lucy"
+      >
+
+        <View style={styles.image}>
+          <Image
+            source={{ uri: this.props.data.data.image }}
+            style={{ width: 64, height: 64 }}
+          />
+        </View>
+
+        <View style={styles.carac}>
+          <Text style={styles.name}>{this.props.data.data.name}</Text>
+          <Text style={styles.descr}>{this.props.data.data.descr} </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    row: {
-      flex: 1,
-      flexDirection: 'row',
-      padding: 12,
-      
-    },
-    name:{
-        fontSize : 16,
-        lineHeight : 24,
-        letterSpacing : 0.1,
-        color : '#3c3b3b'
-    },
-    descr:{
-         fontSize : 12,
-         lineHeight : 20,
-         color : '#37474F'
-  
-    },
-    image : {
-       width : 64,
-       height : 64,
-
-    },
-    carac :{
-        flex : 2,
-        paddingLeft : 10,
-
-    },
-  
-    
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 12
+  },
+  name: {
+    fontSize: 16,
+    lineHeight: 24,
+    letterSpacing: 0.1,
+    color: '#3c3b3b'
+  },
+  descr: {
+    fontSize: 12,
+    lineHeight: 20,
+    color: '#37474F'
+  },
+  image: {
+    width: 64,
+    height: 64
+  },
+  carac: {
+    flex: 2,
+    paddingLeft: 10
+  }
 });
-
-
--- index.android.js
+++ index.android.js
@@ -1,98 +1,108 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, ListView , StyleSheet} from 'react-native';
-import { Container, Header,Left,Content, Button, Text,Icon , Body, Title, Right , Footer, FooterTab, Badge} from 'native-base';
+import { AppRegistry, View, ListView, StyleSheet } from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Content,
+  Button,
+  Text,
+  Icon,
+  Body,
+  Title,
+  Right,
+  Footer,
+  FooterTab,
+  Badge
+} from 'native-base';
 import { StackNavigator } from 'react-navigation';
 import Row from './Component/row';
 import Normativas from './Mock/normativas.json';
-import Pagina from './Pages/paginanorma';  
-
+import Pagina from './Pages/paginanorma';
 
 class Home extends Component {
+  // Taking off default header of navigation
+  static navigationOptions = {
+    header: null
+  };
 
-    // Taking off default header of navigation
-    static navigationOptions = {
-        header : null
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    // Getting mock
+    var customData = require('./Mock/normativas.json');
+    this.state = {
+      dataSource: ds.cloneWithRows(customData)
     };
-
-    constructor(props){
-
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        
-        // Getting mock
-        var customData = require('./Mock/normativas.json');
-        this.state = {
-            dataSource: ds.cloneWithRows(customData)
-        };
-    }    
-
- render() {
-     const { navigate }  = this.props.navigation;    
-
-     return (
-        <Container >
-
-                <Header>
-                    <Left>
-                        <Button transparent >
-                            <Icon name='menu' />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>Normas Header</Title>
-                    </Body>
-                    <Right />
-                </Header>
-
-                <Content>              
-                    <ListView
-                        dataSource={this.state.dataSource}
-                        renderRow={(data) =>                        
-                    <Row data={{ x : this.props , data}}>                      
-                        </Row>
-                        }
-                        renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-                    />
-                </Content>
-
-                <Footer>
-                    <FooterTab>
-                        <Button badge vertical>
-                            <Badge><Text>2</Text></Badge>
-                            <Icon name="apps" />
-                            <Text>Opções</Text>
-                        </Button>
-                        <Button active badge vertical>
-                            <Badge ><Text>51</Text></Badge>
-                            <Icon active name="navigate" />
-                            <Text>Não lidas</Text>
-                        </Button>
-                        <Button>
-                            <Icon name="person" />
-                            <Text>Contatos</Text>
-                        </Button>
-                    </FooterTab>
-                </Footer>
-            </Container>
-      );
-    }
   }
-  
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <Container>
+
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Normas Header</Title>
+          </Body>
+          <Right />
+        </Header>
+
+        <Content>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={data => <Row data={{ x: this.props, data }} />}
+            renderSeparator={(sectionId, rowId) => (
+              <View key={rowId} style={styles.separator} />
+            )}
+          />
+        </Content>
+
+        <Footer>
+          <FooterTab>
+            <Button badge vertical>
+              <Badge><Text>2</Text></Badge>
+              <Icon name="apps" />
+              <Text>Opções</Text>
+            </Button>
+            <Button active badge vertical>
+              <Badge><Text>51</Text></Badge>
+              <Icon active name="navigate" />
+              <Text>Não lidas</Text>
+            </Button>
+            <Button>
+              <Icon name="person" />
+              <Text>Contatos</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
 // Settings from separator
 const styles = StyleSheet.create({
-    separator: {
+  separator: {
     flex: 1,
     height: StyleSheet.hairlineWidth,
-    backgroundColor: '#ddd',
-  },
+    backgroundColor: '#ddd'
+  }
 });
-
 
 // Settings about routering
 const awesome = StackNavigator({
   Home: { screen: Home },
-  Pagina : {screen : Pagina}
+  Pagina: { screen: Pagina }
 });
-
 
 AppRegistry.registerComponent('awesome', () => awesome);
